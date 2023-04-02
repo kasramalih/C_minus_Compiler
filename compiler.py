058c6c2f3a455784c942b73b805e02509acf5385
kasramalih <kasramalihi@gmail.com>
2023-04-02 14:28:07 +0330

    some bugs  fixed, remaining  bugs: 1-  line numbers are sometimes  wrong 2- / is invalid input!

--- compiler.py
+++ compiler.py
@@ -3,22 +3,25 @@ class Scanner:
         self.whitespaces = {'\n', '\r', '\t', '\v', '\f', ' '}
         self.symbols = {';', ':', ',', '[', ']', '(', ')', '{', '}', '+', '-', '*', '=', '<', '>', '/'}
         self.keywords = {'if', 'else', 'void', 'int', 'repeat', 'break', 'until', 'return'}
-        self.symbol_table = []
+        self.symbol_table = ['break', 'else', 'if', 'int', 'repeat', 'return', 'until', 'void']
         self.tokens = {}
         self.errors = {}
         self.line_counter = 1
 
     def write_to_files(self):
         tokens_txt = open('tokens.txt', 'w+')
-        tokens_txt.write('\n'.join([f"{line_no}.\t\t{' '.join(value)}" for line_no, value in self.tokens.items()]))
+        tokens_txt.write('\n'.join([f"{line_no}.\t{' '.join(value)}" for line_no, value in self.tokens.items()]))
         tokens_txt.close()
 
         symbol_table_txt = open('symbol_table.txt', 'w+')
-        symbol_table_txt.write('\n'.join([f'{i}.\t\t{s}' for i, s in enumerate(self.symbol_table)]))
+        symbol_table_txt.write('\n'.join([f'{i+1}.\t{s}' for i, s in enumerate(self.symbol_table)]))
         symbol_table_txt.close()
 
         lexical_errors_txt = open('lexical_errors.txt', 'w+')
-        lexical_errors_txt.write('\n'.join([f"{line_no}.\t\t{' '.join(value)}" for line_no, value in self.errors.items()]))
+        if len(self.errors) == 0:
+            lexical_errors_txt.write('There is no lexical error.')
+        else:
+            lexical_errors_txt.write('\n'.join([f"{line_no}.\t{' '.join(value)}" for line_no, value in self.errors.items()]))
         lexical_errors_txt.close()
 
     def get_lookahead(self, i, chars):
@@ -113,7 +116,7 @@ class Scanner:
         i = 0
         while i < len(lines):
             token, lexeme, new_i = self.get_next_token(lines, i)
-            if token != 'WHITESPACE' and token != 'COMMENT':
+            if token != 'WHITESPACE' and token != 'COMMENT' and token != 'FILE_ENDED':
                 self.add_token(token, lexeme)
             if token == 'FILE_ENDED':
                 break
