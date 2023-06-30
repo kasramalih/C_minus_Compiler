fe9e744f4f8b2f71012e6f099782617484cbb792
kasramalih <kasramalihi@gmail.com>
2023-06-30 20:17:32 +0330

    emtiazi fulllllll

--- codegen.py
+++ codegen.py
@@ -67,6 +67,7 @@ class Codegen:
             second = self.ss.pop()
             op = self.ss.pop()
             first = self.ss.pop()
+            self.type_checker(first, second, line_no)
             temp_addr = self.gettemp()
             self.add_and_increment_pb(generate_3address_code(op, first, second, temp_addr))
             self.ss.append(temp_addr)
@@ -83,7 +84,7 @@ class Codegen:
             self.iter_s.pop()
         elif action == "#break":
             if not self.iter_s:
-                self.semantic_errors.append(f"#{line_no - 1}: Semantic Error! No 'repeat ... until' found for 'break'")
+                self.semantic_errors.append(f"#{line_no}: Semantic Error! No 'repeat ... until' found for 'break'.")
             self.break_s.append(self.i)
             self.pb.append('')
             self.i += 1
@@ -113,6 +114,11 @@ class Codegen:
         elif action == '#defined_check':  # checks for definition of variables only
             if lexeme not in self.st.keys() and lexeme != 'output':
                 self.semantic_errors.append(f"#{line_no}: Semantic Error! '{lexeme}' is not defined.")
+                temp_addr = self.gettemp()
+                self.ss.append(temp_addr)
+            else:
+                addr = str(self.findaddr(lexeme))
+                self.ss.append(addr)
         elif action == '#scope_in':
             function_lexeme_address = self.ss.pop()
             function_lexeme = self.ss.pop()
@@ -121,35 +127,12 @@ class Codegen:
             self.st[function_lexeme] = [self.scope, function_lexeme_address, return_type, self.i, no_args, []]
             self.scope = 1
             self.ss.append(function_lexeme)
-            """On this action:
-                1- create pointer to a new symbol table for the function's scope
-                2- set return type (already in stack, may need to remove the #ignore_void_check action) 
-                in global symbol table
-                3- set jump address in global symbol table
-                4- change current symbol table to the new one
-            """
         elif action == '#scope_out':
-            """
-            print('ALL VARS IN SCOPE:\n-----------------------------')
-            for k in self.st.keys():
-                print(k, self.st[k])
-            """
             for k, v in list(self.st.items()):
                 if v[0] == self.scope:
                     del self.st[k]
             self.scope = 0
-            """
-            print("---------------------\n AFTER SCOPE:\n------------------------")
-            for k in self.st.keys():
-                print(k, self.st[k])
-            print('-----------------------------')
-            """
             self.ss.pop()
-            """On this action:
-                1- change current symbol table to global
-                2- set number of arguments in global symbol table
-                (may need a new action for this after param declarations)
-            """
         elif action == '#param_first':
             address = self.ss.pop()
             function_lexeme = self.ss[-1]
@@ -162,31 +145,12 @@ class Codegen:
             function_lexeme = self.ss[-1]
             self.st[function_lexeme][-2] += 1
             self.st[function_lexeme][-1].append([type, param])
-            """On this action:
-                1- added type, name, reg, index of param into symbol table
-                2- update total number of params(can be handled in stack)
-            """
         elif action == '#arr_param':
             function_lexeme = self.ss[-1]
             self.st[function_lexeme][-1][-1][0] = 'array'
             for k in self.st.keys():
                 if str(self.st[k][1]) == str(self.st[function_lexeme][-1][-1][1]):
                     self.st[k].append('array')
-            """On this action:
-                1- added type, name, reg, index of param into symbol table
-                2- update total number of arguments(can be handled in stack)
-            """
-        elif action == '#check_arg':
-            # TODO
-            """On this action:
-                1- check for type match
-                In next phase we also need to assign value to registers
-            """
-        elif action == '#check_invoke_out':
-            # TODO
-            """On this action:
-                1- check total number of args
-            """
         elif action == '#init_arg_check':
             func_addr = self.ss.pop()
             for k in self.st.keys():
@@ -207,12 +171,14 @@ class Codegen:
                         arg_type = self.st[k][2]
                     except:
                         arg_type = 'int'
+            if cnt > self.st[func_name][-2]:
+                self.ss.append(cnt)
+                return
             if self.st[func_name][-1][cnt - 1][0] != arg_type:
                 self.semantic_errors.append(
-                    f"#{line_no}: Semantic Error! Mismatch in type of argument '{cnt}' of '{func_name}'. Expected '{self.st[func_name][-1][cnt - 1][0]}' but got '{arg_type}' instead")
+                    f"#{line_no}: Semantic Error! Mismatch in type of argument {cnt} of '{func_name}'. Expected '{self.st[func_name][-1][cnt - 1][0]}' but got '{arg_type}' instead.")
             self.ss.append(cnt)
         elif action == "#check_count":
-            # need return address
             total = self.ss.pop()
             func_name = self.ss.pop()
             if self.st[func_name][2] == 'int':
@@ -220,6 +186,8 @@ class Codegen:
             if total != self.st[func_name][-2]:
                 self.semantic_errors.append(
                     f"#{line_no}: Semantic Error! Mismatch in numbers of arguments of '{func_name}'.")
+        elif action == "#pop_return":
+            self.ss.pop()
         else:
             print("ridiiiiiiii")
 
