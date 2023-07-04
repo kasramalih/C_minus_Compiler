834ed1980feb0eed1be31fa5ee3acfd46fa4561f
kasramalih <kasramalihi@gmail.com>
2023-07-04 17:09:05 +0330

    benazaret tamome????

--- codegen.py
+++ codegen.py
@@ -13,10 +13,11 @@ class Codegen:
         self.semantic_errors = []
         self.scope = 0
         # scope, add, iadd, type, start of  func, return add, return val, no.arg, params
-        self.st['output'] = [0, self.current_available_address - 8, self.current_indirect_address - 4, 'void', None,
-                             None, None, 1, [['int', 504]]]
-        self.st['main'] = [0, self.current_available_address - 16, self.current_indirect_address - 12, 'void', 0,
-                           0, 0, 0, [['int', 504]]]
+        self.st[('output', 0)] = [0, self.current_available_address - 8, self.current_indirect_address - 4, 'void',
+                                  None,
+                                  None, None, 1, [['int', 504]]]
+        self.st[('main', 0)] = [0, self.current_available_address - 16, self.current_indirect_address - 12, 'void', 0,
+                                0, 0, 0, [['int', 504]]]
         self.iter_s = []
         self.fst = []
         self.globals_finish_i = 0
@@ -42,12 +43,15 @@ class Codegen:
             d. optional: In caller, load all symbols from stack
         """
         if action == "#pid":
-            addr, indirect_addr = self.findaddr(lexeme)
+            addr, indirect_addr = self.findaddr((lexeme, self.scope), False)
+            self.ss.append('@' + addr)
+        elif action == "#d_pid":
+            addr, indirect_addr = self.findaddr((lexeme, self.scope), True)
             self.ss.append('@' + addr)
         elif action == "#assign_initial_var":
             self.add_and_increment_pb(generate_3address_code('ASSIGN', '#0', self.ss.pop()))
         elif action == "#declare_pid":
-            addr, indirect_addr = self.findaddr(lexeme)
+            addr, indirect_addr = self.findaddr((lexeme, self.scope), True)
             # assign #500, 3000
             self.add_and_increment_pb(generate_3address_code('ASSIGN', '#' + str(indirect_addr), addr))
             self.ss.append('@' + addr)
@@ -162,9 +166,9 @@ class Codegen:
                 return_address = self.gettemp()
                 return_val = self.gettemp()
                 self.fst.append((return_address, return_val))
-                self.st[function_lexeme] = [self.scope, function_lexeme_address[1:], return_type, self.i,
-                                            return_address,
-                                            return_val, no_args, []]
+                self.st[(function_lexeme, 0)] = [self.scope, function_lexeme_address[1:], return_type, self.i,
+                                                 return_address,
+                                                 return_val, no_args, []]
             else:
                 self.fst.append((0, 0))
             self.scope = 1
@@ -185,26 +189,26 @@ class Codegen:
             address = self.ss.pop()
             function_lexeme = self.ss[-1]
             indir_address = None
-            self.st[function_lexeme][-2] += 1
+            self.st[(function_lexeme, 0)][-2] += 1
             for k in self.st.keys():
                 if str(self.st[k][1]) == str(address[1:]):
                     indir_address = self.st[k][2]
-            self.st[function_lexeme][-1].append(['int', address, str(indir_address)])
+            self.st[(function_lexeme, 0)][-1].append(['int', address, str(indir_address)])
         elif action == '#param':
             param = self.ss.pop()
             param_lexeme = self.ss.pop()
             type = self.ss.pop()
             function_lexeme = self.ss[-1]
             indir_address = None
-            self.st[function_lexeme][-2] += 1
+            self.st[(function_lexeme, 0)][-2] += 1
             for k in self.st.keys():
                 if str(self.st[k][1]) == str(param[1:]):
                     indir_address = self.st[k][2]
-            self.st[function_lexeme][-2] += 1
-            self.st[function_lexeme][-1].append([type, param, indir_address])
+            self.st[(function_lexeme, 0)][-2] += 1
+            self.st[(function_lexeme, 0)][-1].append([type, param, indir_address])
         elif action == '#arr_param':
             function_lexeme = self.ss[-1]
-            self.st[function_lexeme][-1][-1][0] = 'array'
+            self.st[(function_lexeme, 0)][-1][-1][0] = 'array'
         elif action == '#init_arg_check':
             # self.add_and_increment_pb(generate_3address_code('PRINT', '#5555555'))
             func_addr = self.ss.pop()
@@ -217,8 +221,8 @@ class Codegen:
                 pass
             for k in self.st.keys():
                 if str(self.st[k][1]) == str(func_addr[1:]):
-                    func_name = k
-                    self.ss.append(k)
+                    func_name, scope = k
+                    self.ss.append(func_name)
             self.ss.append(0)
             if func_name == 'output':
                 return
@@ -265,14 +269,15 @@ class Codegen:
             if func_name == 'output':
                 self.add_and_increment_pb(generate_3address_code('PRINT', arg))
             else:
-                if self.st[func_name][-1][cnt - 1][0] == 'array':
+                if self.st[(func_name, 0)][-1][cnt - 1][0] == 'array':
                     self.add_and_increment_pb(
-                        generate_3address_code('ASSIGN', arg[1:], self.st[func_name][-1][cnt - 1][1][1:]))
+                        generate_3address_code('ASSIGN', arg[1:], self.st[(func_name, 0)][-1][cnt - 1][1][1:]))
                 else:
                     self.add_and_increment_pb(
-                        generate_3address_code('ASSIGN', '#' + str(self.st[func_name][-1][cnt - 1][2]),
-                                               self.st[func_name][-1][cnt - 1][1][1:]))
-                    self.add_and_increment_pb(generate_3address_code('ASSIGN', arg, self.st[func_name][-1][cnt - 1][1]))
+                        generate_3address_code('ASSIGN', '#' + str(self.st[(func_name, 0)][-1][cnt - 1][2]),
+                                               self.st[(func_name, 0)][-1][cnt - 1][1][1:]))
+                    self.add_and_increment_pb(
+                        generate_3address_code('ASSIGN', arg, self.st[(func_name, 0)][-1][cnt - 1][1]))
             self.ss.append(cnt)
         elif action == "#check_count":
             total = self.ss.pop()
@@ -285,7 +290,8 @@ class Codegen:
             # self.add_and_increment_pb(generate_3address_code('PRINT', '#444444444'))
             self.add_and_increment_pb(
                 generate_3address_code('ASSIGN', self.fst[-1][0], '@' + str(self.stack_pointer)))
-            self.add_and_increment_pb(generate_3address_code('ASSIGN', '#' + str(self.i + 2), self.st[func_name][-4]))
+            self.add_and_increment_pb(
+                generate_3address_code('ASSIGN', '#' + str(self.i + 2), self.st[(func_name, 0)][-4]))
             # self.add_and_increment_pb(generate_3address_code('PRINT', '#444444444'))
             # self.add_and_increment_pb(generate_3address_code('PRINT', self.st[func_name][-4]))
             # self.add_and_increment_pb(generate_3address_code('PRINT', '@' + str(self.stack_pointer)))
@@ -293,10 +299,10 @@ class Codegen:
             # self.add_and_increment_pb(generate_3address_code('PRINT', '#444444444'))
 
             # jump to start of function
-            self.add_and_increment_pb(generate_3address_code('JP', self.st[func_name][3]))
-            if self.st[func_name][2] == 'int':
+            self.add_and_increment_pb(generate_3address_code('JP', self.st[(func_name, 0)][3]))
+            if self.st[(func_name, 0)][2] == 'int':
                 t_return = str(self.gettemp())
-                self.add_and_increment_pb(generate_3address_code('ASSIGN', self.st[func_name][-3], t_return))
+                self.add_and_increment_pb(generate_3address_code('ASSIGN', self.st[(func_name, 0)][-3], t_return))
                 self.ss.append(t_return)
                 # self.add_and_increment_pb(generate_3address_code('PRINT', '#999999'))
                 # self.add_and_increment_pb(generate_3address_code('PRINT', '#' + str(self.st[func_name][-3])))
@@ -314,12 +320,21 @@ class Codegen:
         else:
             print("ridiiiiiiii")
 
-    def findaddr(self, inp):
-        if inp not in self.st.keys():
+    def findaddr(self, inp, declare):
+        if declare:
             self.st[inp] = [self.scope, self.current_available_address, self.current_indirect_address]
             self.current_available_address += 4
             self.current_indirect_address += 4
-        return str(self.st[inp][1]), str(self.st[inp][2])
+            return str(self.st[inp][1]), str(self.st[inp][2])
+        else:
+            lexeme, scope = inp
+            if scope == 1:
+                if (lexeme, 1) in self.st.keys():
+                    return str(self.st[(lexeme, 1)][1]), str(self.st[(lexeme, 1)][2])
+                else:
+                    return str(self.st[(lexeme, 0)][1]), str(self.st[(lexeme, 0)][2])
+            else:
+                return str(self.st[(lexeme, 0)][1]), str(self.st[(lexeme, 0)][2])
 
     def gettemp(self):
         addr = self.current_available_address
@@ -329,25 +344,6 @@ class Codegen:
     def add_and_increment_pb(self, code):
         self.pb.append(code)
         self.i += 1
-
-    def type_checker(self, address_op1, address_op2, line_no):
-        type_op1 = ''
-        type_op2 = ''
-        for k in self.st.keys():
-            if str(self.st[k][1]) == str(address_op1):
-                try:
-                    type_op1 = self.st[k][3]
-                except:
-                    type_op1 = 'int'
-            if str(self.st[k][1]) == str(address_op2):
-                try:
-                    type_op2 = self.st[k][3]
-                except:
-                    type_op2 = 'int'
-        if type_op1 == 'array' or type_op2 == 'array':
-            self.semantic_errors.append(
-                f"#{line_no}: Semantic Error! Type mismatch in operands, Got array instead of int.")
-        # 26: Semantic Error! Type mismatch in operands, Got array instead of int.
 
     def retrieve_values(self):
         t = self.gettemp()
